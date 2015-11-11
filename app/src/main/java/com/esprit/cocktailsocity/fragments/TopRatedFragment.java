4e2a643d82bc2eb3d9a967f75e3e658ee1053f62
dalitoo <mohamedali.abdallah@esprit.tn>
2015-11-11 17:32:15 +0100

    Graphic interfaces

---
+++ app/src/main/java/com/esprit/cocktailsocity/fragments/TopRatedFragment.java
@@ -0,0 +1,33 @@
+package com.esprit.cocktailsocity.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.esprit.cocktailsocity.R;
+
+
+public class TopRatedFragment extends Fragment {
+
+
+    public TopRatedFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top_rated, container, false);
+    }
+
+}
