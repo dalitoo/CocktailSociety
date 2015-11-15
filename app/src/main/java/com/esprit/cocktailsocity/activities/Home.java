e958d138ef7b954f609a6990027ce802dc3e3686
dalitoo <mohamedali.abdallah@esprit.tn>
2015-11-15 01:02:27 +0100

    Graphic interfaces

--- app/src/main/java/com/esprit/cocktailsocity/activities/Home.java
+++ app/src/main/java/com/esprit/cocktailsocity/activities/Home.java
@@ -1,23 +1,18 @@
 package com.esprit.cocktailsocity.activities;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 import com.esprit.cocktailsocity.R;
 import com.esprit.cocktailsocity.fragments.FavouriteFragment;
@@ -41,14 +36,15 @@ public class Home extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
-
+    Toolbar toolbar;
+    private int[] tabIcons= {R.mipmap.ic_person_black_24dp,R.mipmap.ic_launcher};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
+       toolbar = (Toolbar) findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -59,6 +55,12 @@ public class Home extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[1]);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +70,7 @@ public class Home extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
 
     }
 
@@ -135,16 +138,16 @@ public class Home extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Home";
-                case 1:
-                    return "Search";
-                case 2:
-                    return "Favourite";
-                case 3:
-                    return "Top Rated";
-            }
+//            switch (position) {
+//                case 0:
+//                    return "Home";
+//                case 1:
+//                    return "Search";
+//                case 2:
+//                    return "Favourite";
+//                case 3:
+//                    return "Top Rated";
+//            }
             return null;
         }
     }
